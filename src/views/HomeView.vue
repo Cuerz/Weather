f915c0a712eeb45c81bb3c778b428c1e0319afdd
Cuerz <sean07260@gmail.com>
2022-11-17 18:23:06 +0800

    add page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,15 @@
 
       <div id="page2" class="scrollbar-demo-item">
         <div class="intro">
+          <div class="link">
+            <router-link to="/nuist" style="text-decoration: none;">
+              Click 解锁四季南信
+            </router-link>
+          </div>
+          <div class="poem">
+            <p v-for="item in poem" :key="item">{{ item }}</p>
+          </div>
+
 
         </div>
       </div>
@@ -21,41 +30,72 @@
 
 
 <style scoped>
-  .scrollbar-demo-item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    text-align: center;
-    color: var(--el-color-primary);
-  }
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
 
-  #page1{
-    position: relative;
-  }
+#page1 {
+  position: relative;
+}
 
-  video {
-    width: 100%;
-    height: 100%;
-    object-fit: fill;
-    z-index: -9999;
-  }
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  z-index: -9999;
+}
 
-  .banner{
-    position: absolute;
-    height: 10vh;
-    user-select: none;
-  }
+.banner {
+  position: absolute;
+  height: 10vh;
+  user-select: none;
+}
+
+.intro {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  object-fit: fill;
+  background: url(../assets/bg.png);
+  background-size: cover;
+}
+
+.poem {
+  position: absolute;
+  font-size: 25px;
+  color: aliceblue;
+  right: 10vw;
+  top: 10vh;
+}
+
+.link {
+  margin-top: 5vh;
+  font-size: 30px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.link>a {
+  color: #42b983;
+}
 </style>
 
 <script>
-  // @ is an alias to /src
+// @ is an alias to /src
 
 
-  export default {
-    name: 'HomeView',
-    components: {
+export default {
+  name: 'HomeView',
+  components: {
 
+  },
+  data() {
+    return {
+      poem: ['春雨惊春清谷天，', '夏满芒夏暑相连。', '秋处露秋寒霜降，', '冬雪雪冬小大寒。']
     }
-  }
+  },
+}
 </script>
