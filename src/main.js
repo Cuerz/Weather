119a8c92b1063b3d8b36fa0734ef6902ed2b58bb
Cuerz <sean07260@gmail.com>
2022-11-17 15:20:26 +0800

    add video

--- src/main.js
+++ src/main.js
@@ -7,4 +7,5 @@ import 'element-plus/dist/index.css'
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-scrollbar height="100vh">
+      <div id="page1" class="scrollbar-demo-item">
+        <video loop="loop" autoplay="autoplay" muted="muted">
+          <source src="../assets/autumn.mp4" type="video/mp4">
+          <!-- <source src="../assets/nuist.mp4" type="video/mp4"> -->
+        </video>
+        <img class="banner" src="../assets/index.png" />
+      </div>
+
+      <div id="page2" class="scrollbar-demo-item">
+        <div class="intro">
+
+        </div>
+      </div>
+    </el-scrollbar>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+<style scoped>
+  .scrollbar-demo-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
+    color: var(--el-color-primary);
   }
-}
+
+  #page1{
+    position: relative;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+    z-index: -9999;
+  }
+
+  .banner{
+    position: absolute;
+    height: 10vh;
+    user-select: none;
+  }
+</style>
+
+<script>
+  // @ is an alias to /src
+
+
+  export default {
+    name: 'HomeView',
+    components: {
+
+    }
+  }
 </script>
