3b0b50d0c13e93e8e933ed53cb7e595538bb0a6b
Cuerz <sean07260@gmail.com>
2022-12-18 19:41:13 +0800

    modify

--- src/views/FeedBack.vue
+++ src/views/FeedBack.vue
@@ -1,15 +1,66 @@
 <template>
-    <div>
-
-    </div>
+  <div class="main">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>留言反馈</span>
+          <el-button type="text" @click="submit">提交</el-button>
+        </div>
+      </template>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="联系方式">
+          <el-input v-model="form.phone" placeholder="请填写手机号"></el-input>
+        </el-form-item>
+        <el-form-item label="留言">
+          <el-input type="textarea" v-model="form.comment"></el-input>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'FeedBack',
+import { feedback } from '@/api'
+
+export default {
+  name: 'FeedBack',
+  data() {
+    return {
+      form: {
+        phone: '',
+        comment: '',
+      },
     }
+  },
+  methods: {
+    submit() {
+      feedback(this.form).then((response) => {
+        console.log(response)
+      })
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.main {
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  background-color: #8ec5fc;
+  background-image: linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
+}
 
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.box-card {
+  width: 480px;
+  height: 280px;
+  display: block;
+  margin: 15vh auto 0;
+}
 </style>
--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,29 +1,55 @@
 <template>
-  <el-row>
-    <el-col :span="10">
+  <div class="base">
+    <el-row>
+      <el-col :span="10">
         <WeatherCardVue />
-    </el-col>
+      </el-col>
 
-    <el-col :span="14">
-      <el-card style="height:390px;width: 840px;margin-top: 5vh;">
-        <TemperatureChartVue />
-      </el-card>
+      <el-col :span="14">
+        <el-card style="height: 390px; width: 840px; margin-top: 5vh">
+          <TemperatureChartVue />
+        </el-card>
 
-      <el-card style="height:390px;width: 840px;margin-top: 5vh;">
-        <RainChartVue />
-      </el-card>
-    </el-col>
-  </el-row>
+        <el-card style="height: 390px; width: 840px; margin-top: 5vh">
+          <RainChartVue />
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-button
+      type="primary"
+      size="large"
+      :icon="Edit"
+      style="position: absolute; top: 3vh; right: 3vw"
+      @click="goFeedback"
+      >留言反馈</el-button
+    >
+  </div>
 </template>
 
 <script>
 import WeatherCardVue from '@/components/card.vue'
 import TemperatureChartVue from '@/components/TemperatureChart.vue'
-import RainChartVue from '@/components/RainChart.vue';
+import RainChartVue from '@/components/RainChart.vue'
 export default {
   name: 'WeatherView',
-  components: { WeatherCardVue, TemperatureChartVue,RainChartVue },
+  components: { WeatherCardVue, TemperatureChartVue, RainChartVue },
+  methods: {
+    goFeedback() {
+      this.$router.push({ path: '/feedback' })
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<script setup>
+import { Edit } from '@element-plus/icons-vue'
+</script>
+
+<style scoped>
+.base {
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffdee9;
+  background-image: linear-gradient(0deg, #ffdee9 0%, #b5fffc 100%);
+}
+</style>
